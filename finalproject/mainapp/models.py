3db88cacb5176bd3120b834ae34055b7882037fa
Mrkorg1000 <korg1000@yandex.ru>
2022-07-11 16:41:28 +0300

    в проект добавлен debug_toolbar. в models прописаны модели статьи и категории. Для пользователя будет создано отдельное приложение

--- finalproject/mainapp/models.py
+++ finalproject/mainapp/models.py
@@ -1,19 +1,5 @@
 from django.db import models
 
-# Модель пользователя.
-# Закомментировал пока поле АВАТАР,
-# т.к. требует доп настроек (сделаем потом)
-class User(models.Model):
-    user_name = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    # avatar = models.ImageField(upload_to='images') # это папка под имиджи 
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self) -> str:
-        return self.user_name
-
 
 # Модель раздела/категории.
 class Category(models.Model):
