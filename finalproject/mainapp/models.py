233f6b2802d63c9f9ae6b64f5633a192808c1525
Mrkorg1000 <80282966+Mrkorg1000@users.noreply.github.com>
2022-07-11 17:17:38 +0300

    Nick3 (#6)
    * зарегистрировал mainapp  в INSTALLED APPS
    
    * В проект добавлен debug_toolbar (на будущее) и прописаны модели Пользователя, статьи и категории.
    
    * в проект добавлен debug_toolbar. в models прописаны модели статьи и категории. Для пользователя будет создано отдельное приложение

--- finalproject/mainapp/models.py
+++ finalproject/mainapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+# Модель раздела/категории.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+# Модель статьи/публикации.
+class Articles(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        User, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+
