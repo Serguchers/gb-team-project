14f7689c47c027acc7c4f5ee69e34ebc90a82676
Tornado174 <79004731+Tornado174@users.noreply.github.com>
2022-07-11 17:55:41 +0500

    Oleg (#5)
    * main page and menubar
    
    * Template base.html and category

--- finalproject/mainapp/views.py
+++ finalproject/mainapp/views.py
@@ -6,25 +6,27 @@ from django.shortcuts import render
 
 
 def show_main(request):
-    return render(request, '../templates/base.html')
+    return render(request, 'index.html')
 
 def show_design(request):
-    return render(request, '../templates/base.html')
+    return render(request, 'design.html')
 
 def show_web_dev(request):
-    return render(request, '../templates/base.html')
+    return render(request, 'web_dev.html')
 
 def show_mobile_dev(request):
-    return render(request, '../templates/base.html')
+    return render(request, 'mobile_dev.html')
 
 def show_marketing(request):
-    return render(request, '../templates/base.html')
+    return render(request, 'marketing.html')
 
 def registration(request):
-    return render(request, '../templates.base.html')
+    return render(request, 'registration.html')
+
 
 def signin(request):
     return render(request, '../templates.base.html')
 
 def help_page(request):
-    return render(request, '../templates.base.html')
+    return render(request, 'help.html')
+
